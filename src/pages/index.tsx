20bccb0e390e0ed650be595da4e44d51e3852eb7
ThanhHau05 <nguyenthanhhau.contact@gmail.com>
2023-07-26 08:59:11 +0700

    upload source code

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+const Index = () => {
+  return (
+    <div className="relative h-screen w-full bg-slate-300">
+      <div className="absolute top-0 z-10 flex h-full w-full justify-center">
+        <div className="h-full w-[400px] border-x-2 border-slate-50 bg-white shadow-lg" />
+      </div>
+    </div>
+  );
+};
+
+export default Index;
