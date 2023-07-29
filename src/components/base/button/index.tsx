3d2a940263d8747bd3013f0ca01c6b6e5702bb47
ThanhHau05 <nguyenthanhhau.contact@gmail.com>
2023-07-29 23:16:54 +0700

    update welcomepage, fix bugs button and input

--- src/components/base/button/index.tsx
+++ src/components/base/button/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import Link from 'next/link';
 import React from 'react';
 
@@ -6,18 +7,30 @@ export const Button = ({
   onClick,
   disabled,
   to,
+  bgWhite,
 }: {
   title: string;
   onClick?: () => void;
   disabled?: boolean;
   to?: string;
+  bgWhite?: boolean;
 }) => {
   return to ? (
     <Link href={to}>
-      <ContainerButton title={title} onClick={onClick} disabled={disabled} />
+      <ContainerButton
+        title={title}
+        onClick={onClick}
+        disabled={disabled}
+        bgWhite={bgWhite}
+      />
     </Link>
   ) : (
-    <ContainerButton title={title} onClick={onClick} disabled={disabled} />
+    <ContainerButton
+      title={title}
+      onClick={onClick}
+      disabled={disabled}
+      bgWhite={bgWhite}
+    />
   );
 };
 
@@ -25,16 +38,21 @@ export const ContainerButton = ({
   title,
   onClick,
   disabled,
+  bgWhite,
 }: {
   title: string;
   onClick?: () => void;
   disabled?: boolean;
+  bgWhite?: boolean;
 }) => {
   return (
     <button
       disabled={disabled}
       onClick={onClick}
-      className="h-full w-full rounded-full border-2 border-gray-300 bg-blue-600 text-sm font-medium text-white shadow-md drop-shadow-md transition-all hover:shadow-xl disabled:cursor-not-allowed disabled:bg-slate-400 disabled:hover:shadow-md"
+      className={clsx(
+        'h-full w-full rounded-full text-sm font-bold shadow-md drop-shadow-md transition-all hover:shadow-xl disabled:cursor-not-allowed disabled:bg-slate-400 disabled:hover:shadow-md',
+        bgWhite ? 'bg-white text-gray-900' : 'bg-blue-600 text-white',
+      )}
     >
       {title}
     </button>
---
+++ src/components/images/welcome/index.ts
@@ -0,0 +1,9 @@
+import Background2 from './5831236.png';
+import Background from './background.png';
+import Background3 from './background3.jpg';
+
+export const ImagesHome = {
+  Background,
+  Background2,
+  Background3,
+};
