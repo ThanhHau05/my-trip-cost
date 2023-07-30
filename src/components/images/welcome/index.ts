99f39c05aaaf6b6e684f9cc035e0254cc66f27f6
ThanhHau05 <nguyenthanhhau.contact@gmail.com>
2023-07-30 20:11:56 +0700

    change some things

--- src/components/images/welcome/index.ts
+++ src/components/images/welcome/index.ts
@@ -1,9 +1,11 @@
 import Background2 from './5831236.png';
 import Background from './background.png';
 import Background3 from './background3.jpg';
+import Background4 from './background4.jpg';
 
 export const ImagesHome = {
   Background,
   Background2,
   Background3,
+  Background4,
 };
