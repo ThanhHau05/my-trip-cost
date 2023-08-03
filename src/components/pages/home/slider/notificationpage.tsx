86a7be15b3d25b64915ca45abc08d0f6af9b341f
ThanhHau05 <nguyenthanhhau.dev@gmail.com>
2023-08-03 16:21:51 +0700

    update component invitation

--- src/components/pages/home/slider/notificationpage.tsx
+++ src/components/pages/home/slider/notificationpage.tsx
@@ -1,5 +1,8 @@
 import { BsCardList } from 'react-icons/bs';
 
+import { Invitation } from '@/components/base/invitation';
+import type { SelectOptionsInvitation } from '@/constants/select-options';
+
 export const NotificationPage = () => {
   return (
     <div className="h-full w-full rounded-t-[40px] bg-white">
@@ -8,6 +11,29 @@ export const NotificationPage = () => {
           There is no notification or invitation for any trip
         </h2>
         <BsCardList className="text-4xl text-gray-800 drop-shadow-md" />
+      </div>
+    </div>
+  );
+};
+
+export const RenderInvitation = ({
+  data,
+}: {
+  data: SelectOptionsInvitation[];
+}) => {
+  return (
+    <div className="h-full w-full py-2">
+      <div className="scroll_invitation flex h-full flex-col items-center justify-start gap-2 overflow-auto rounded-3xl px-2">
+        {data.map((item) => (
+          <Invitation
+            key={item.tripid}
+            date={item.date}
+            name={item.name}
+            time={item.time}
+            tripid={item.tripid}
+            tripname={item.tripname}
+          />
+        ))}
       </div>
     </div>
   );
