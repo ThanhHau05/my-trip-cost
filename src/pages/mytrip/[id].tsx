f2d4783144218a3213a2a5a05197e69175288c60
ThanhHau05 <nguyenthanhhau.dev@gmail.com>
2023-08-09 20:43:18 +0700

    update form invoice

--- src/pages/mytrip/[id].tsx
+++ src/pages/mytrip/[id].tsx
@@ -1,6 +1,11 @@
 import { useContext } from 'react';
 
-import { AmountOfMoneyOfUser, Button, VerticalMenu } from '@/components/base';
+import {
+  AmountOfMoneyOfUser,
+  Button,
+  Invoice,
+  VerticalMenu,
+} from '@/components/base';
 import { TripHeader, WrapperHeader } from '@/components/layout';
 import { MainContext } from '@/context/main-context';
 
@@ -29,6 +34,50 @@ const TripDetail = () => {
           </div>
         </VerticalMenu>
       ) : null}
+      <div className="h-full w-full px-3 pt-5">
+        <div className="dropdown h-[calc(100%-73px)] w-full overflow-auto pr-2">
+          <div className="flex items-center">
+            <div className="ml-[18px] mr-3 inline-block h-3 w-3 rounded-full bg-gray-800" />
+            <div className="flex flex-col">
+              <span className="text-lg">Start the trip</span>
+              <span>10.00 PM - 09/08/2023</span>
+            </div>
+          </div>
+          <Invoice
+            name="HauNguyen"
+            activity="Shopping"
+            qty={2}
+            description="buy a water bottle"
+            money={50000}
+            time="10.00 PM - 09/08/2023"
+            url="https://lh3.googleusercontent.com/a/AAcHTtdb5ZItA3BDojhwftTd3N2UNjqyeIe5Fww1rX5ZWj4=s360-c-no"
+            color="123455"
+            text="H"
+          />
+          <Invoice
+            name="HauNguyen"
+            activity="Shopping"
+            qty={2}
+            money={50000}
+            time="10.00 PM - 09/08/2023"
+            url="https://lh3.googleusercontent.com/a/AAcHTtdb5ZItA3BDojhwftTd3N2UNjqyeIe5Fww1rX5ZWj4=s360-c-no"
+            color="123455"
+            text="H"
+          />
+          <Invoice
+            name="HauNguyen"
+            activity="Shopping"
+            money={50000}
+            time="10.00 PM - 09/08/2023"
+            url="https://lh3.googleusercontent.com/a/AAcHTtdb5ZItA3BDojhwftTd3N2UNjqyeIe5Fww1rX5ZWj4=s360-c-no"
+            color="123455"
+            text="H"
+          />
+        </div>
+        <div className="mt-3 h-12 w-full">
+          <Button title="Add Invoice" />
+        </div>
+      </div>
     </WrapperHeader>
   );
 };
