b1fef5d6fccd3cff3b8ee1c45b710724a13e5c82
ThanhHau05 <nguyenthanhhau.dev@gmail.com>
2023-08-30 20:50:01 +0700

    fix bugs some things

--- src/components/pages/home/create-the-trip/create-the-trip.tsx
+++ src/components/pages/home/create-the-trip/create-the-trip.tsx
@@ -9,15 +9,17 @@ export const CreateTheTrip = ({ show }: { show: boolean }) => {
   const { setShowCreateTheTrip } = useContext(MainContext);
 
   return show ? (
-    <div className="fixed z-20 h-full w-full rounded-[40px] bg-purple-200 p-2 sm:w-[400px]">
-      <div className="relative h-full w-full rounded-t-[40px] border bg-purple-50 p-3 shadow-md">
-        <div className="flex w-full justify-end">
-          <IoClose
-            className="cursor-pointer text-3xl text-gray-900"
-            onClick={() => setShowCreateTheTrip(false)}
-          />
+    <div className="fixed top-0 z-20 h-full w-full bg-slate-700/20 pt-20 sm:w-[400px]">
+      <div className="h-full w-full rounded-t-[40px] bg-purple-200 p-2">
+        <div className="relative h-full w-full rounded-t-[40px] border bg-purple-50 p-3 shadow-md">
+          <div className="flex w-full justify-end">
+            <IoClose
+              className="cursor-pointer text-3xl text-gray-900"
+              onClick={() => setShowCreateTheTrip(false)}
+            />
+          </div>
+          <OptionsCreateTheTrip />
         </div>
-        <OptionsCreateTheTrip />
       </div>
     </div>
   ) : null;
